8c3c9377f1b8325b96c3211fb0ad286f0e7faac6
psnider30 <pbsnider@gmail.com>
2018-03-03 17:46:49 -0800

    finish auth.rb

--- lib/auth.rb
+++ lib/auth.rb
@@ -2,11 +2,26 @@ require 'jwt'
 
 class Auth
 
-  def self.issue(payload)
+  ALGORITHM = 'HS256'
 
+  def self.issue(payload)
+    JWT.encode(
+      payload,
+      auth_secret,
+      ALGORITHM
+    )
   end
 
   def self.decode(token)
+    JWT.decode(
+      token,
+      auth_secret,
+      true,
+      { algorithm: ALGORITHM }
+    ).first
+  end
 
+  def self.auth_secret
+    ENV["AUTH_SECRET"]
   end
 end
