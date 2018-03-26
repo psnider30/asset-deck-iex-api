6e8fa5ac8ec21a653cedaa1108ca010fd1c6028b
psnider30 <pbsnider@gmail.com>
2018-03-26 13:14:07 -0700

    Remove uuid from user_assets#update json response.

--- app/controllers/api/user_assets_controller.rb
+++ app/controllers/api/user_assets_controller.rb
@@ -7,7 +7,7 @@ class Api::UserAssetsController < ApplicationController
       user_asset = UserAsset.find_by(user_id: user.id, asset_id: asset.id)
       asset_params[:transaction] == 'buy' ? user_asset.add_share : user_asset.remove_share
       if user_asset.save
-        render json: { symbol: asset.symbol, shares: user_asset.shares, uuid: asset_params[:uuid] }
+        render json: { symbol: asset.symbol, shares: user_asset.shares }
       else
         render json:{ shares: 0, errors: { message: "Share Transaction Failed" } }
       end
