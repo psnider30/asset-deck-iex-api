0631eded450e3864f925f4c3faa60dc7a1a0a9ec
psnider30 <pbsnider@gmail.com>
2018-04-02 11:27:42 -0700

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000' # <- Create React App port URL
+    origins '*' # <- Create React App port URL and heroku app url
 
     resource '*',
       headers: :any,
