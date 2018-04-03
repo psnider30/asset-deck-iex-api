9ce5a75b3f3d60827ef665ed42d612a5c3a97946
psnider30 <pbsnider@gmail.com>
2018-04-03 11:45:44 -0700

    Move sessions and registations controller out from under api name spacing

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "welcome#index"
+  # root "welcome#index"
+  post '/login', to: "sessions#create"
+  post '/signup', to: "registrations#create"
   namespace :api do
     resources :assets, only: [:create]
 
-    post '/login', to: "sessions#create"
-    post '/signup', to: "registrations#create"
     delete '/assets/delete', to: "assets#destroy"
     put '/assets/update', to: "assets#update"
     post '/assets/user-assets', to: "assets#index"
